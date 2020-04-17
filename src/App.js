885c3f5b726582c1cad3aa3554338eabae7bca15
nesrine3ayadi <nesrine3ayadi@gmail.com>
2020-04-17 09:56:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
 import React, { Component } from 'react'
 import './App.css';
 import MoviePage from './pages/MoviePage'
-import Spinner from './components/Spinner'
 
 
 
 
 export default class App extends Component {
 
-  state = {
-    isLoading: true
-  }
-
-  componentDidMount() {
-    setTimeout(
-      () => {
-        this.setState({
-          isLoading: !this.state.isLoading
-        })
-      }, 3000
-    )
-    
-  }
-
+ 
 
   render() {
     return (
       <div className="App" >
-        {
-          this.state.isLoading ? <Spinner /> : <MoviePage />
-        }
+         <MoviePage />
+        
       </div>
     )
   }
